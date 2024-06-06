c09d3221e51c73330c0b1ede13f0b46a6904a30e
BeckySosa <156535491+BeckySosa@users.noreply.github.com>
2024-06-06 19:27:38 -0300

    vamos a ver!

--- src/accesoADatos/MembresiaData.java
+++ src/accesoADatos/MembresiaData.java
@@ -7,6 +7,10 @@ package accesoADatos;
 import entidades.Membresia;
 import entidades.Socio;
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -18,9 +22,16 @@ import javax.swing.JOptionPane;
 public class MembresiaData {
 
     private Connection conn = null;
-
+     MembresiaData membresiaData;
+         SocioData socioData;
+         Socio socio;
+         Membresia membresia;
     public MembresiaData() {
         conn = Conexion.getConexion();
+       membresiaData=new MembresiaData();
+       membresia=new Membresia();
+       socio=new Socio();
+       socioData=new SocioData();
     }
    public Membresia buscarMembresiaActiva(int idSocio){
         String sql= "SELECT * FROM  membresia WHERE estado = 1 AND idSocio = ? ";
@@ -60,7 +71,7 @@ public class MembresiaData {
 //        mem = buscarMembresiaActiva();
 //        int cont = mem.getCantidadPases();
 //        
-        int cont = buscarMembresiaActiva().getCantidadPases();
+        int cont = buscarMembresiaActiva(idSocio).getCantidadPases();
         String sql = "UPDATE membresia SET pases = ? WHERE idSocio = ?";
         
         try {
@@ -122,5 +133,64 @@ public class MembresiaData {
         }
     }
     
+public boolean sonIgualesLasFechas(LocalDate tiempoFin){
+        boolean bandera=false;
+        LocalDate hoy= LocalDate.now();
+        if (hoy.isBefore(tiempoFin)) {
+        bandera=false;
+        } else if (hoy.isAfter(tiempoFin)) {
+           bandera=false;
+        } else {
+            bandera=true;
+        }
 
+        return bandera;
+    }
+public void modificarMembresiasVencidas(){
+    List<Membresia> membresias = buscarMembresiasActivas();
+    for (Membresia membresia1 : membresias) {
+        boolean bandera= membresiaData.sonIgualesLasFechas(membresia1.getFechaFin());
+        if(bandera){
+          String sql = "UPDATE membresia SET estado = 0 WHERE idMembresia = ?";
+         try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, membresia1.getIdMembresia());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                membresia1.setIdMembresia(rs.getInt("idMembresia"));
+                membresia1.setFechaFin(rs.getDate("fechaFin").toLocalDate());
+                membresia1.setEstado(rs.getBoolean("estado"));
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla membresia");
+        }  
+        }
+    }
+     
+    
+    
+       
+    }  
+public List<Membresia> buscarMembresiasActivas(){
+           String sql= "SELECT idMembresia , fechaFin , estado FROM  membresia WHERE estado = ? ";
+        List<Membresia> membresias = new ArrayList<>();
+         try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setBoolean(1, true);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                membresia.setIdMembresia(rs.getInt("idMembresia"));
+                membresia.setFechaFin(rs.getDate("fechaFin").toLocalDate());
+                membresia.setEstado(rs.getBoolean("estado"));
+                membresias.add(membresia);
+                
+            }
+          
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla membresia");
+        }
+         return membresias;
+}
 }
